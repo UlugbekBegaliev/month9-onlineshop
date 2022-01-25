63c33ec88b2cb5e168477a6834e5888b7d8a2dac
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-25 17:55:02 +0600

    init commit

---
+++ src/test/java/com/begaliev/month9onlineshop/Month9OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begaliev.month9onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Month9OnlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
