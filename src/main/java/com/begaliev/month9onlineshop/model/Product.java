ce36592d87fa8274a8e24e9588052acd41fe8d99
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-27 15:25:34 +0600

    added Product model

---
+++ src/main/java/com/begaliev/month9onlineshop/model/Product.java
@@ -0,0 +1,36 @@
+package com.begaliev.month9onlineshop.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128)
+    private String name;
+
+    @Column(length = 300)
+    private String description;
+
+    @Column(length = 128)
+    private String image;
+
+    @Column(length = 128)
+    private int quantity;
+
+    @NotNull
+    @Column(length = 128)
+    private float price;
+}
