1fa5eb62f15cc705d23308ff0e86aee2967de96e
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-27 16:02:27 +0600

    added ProductService and ProductRepository

---
+++ src/main/java/com/begaliev/month9onlineshop/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.begaliev.month9onlineshop.service;
+
+import com.begaliev.month9onlineshop.dto.ProductDTO;
+import com.begaliev.month9onlineshop.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Page<ProductDTO> getAll(Pageable pageable){
+        return productRepository.findAll(pageable).map(ProductDTO::from);
+    }
+
+    public ProductDTO getById(Integer id){
+        return ProductDTO.from(productRepository.findById(id).get());
+    }
+
+    public Page<ProductDTO> getAllByTypeId(int id, Pageable pageable){
+        return productRepository.findAllByTypeId(id, pageable).map(ProductDTO::from);
+    }
+}
