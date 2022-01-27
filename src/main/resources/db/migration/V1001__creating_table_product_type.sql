d732b5a57a3d0bec1bcdb8cb6982cf2e48d5d0c9
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-27 17:16:59 +0600

    added product_types migration file

---
+++ src/main/resources/db/migration/V1001__creating_table_product_type.sql
@@ -0,0 +1,11 @@
+use `onlineshop`;
+    CREATE TABLE `product_types` (
+    `id` INT auto_increment NOT NULL,
+    `name` VARCHAR(128) NOT NULL,
+    `icon` VARCHAR(128) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `products`
+ADD COLUMN `product_type_id` INT NOT NULL after `image`, ADD CONSTRAINT `fk_product__product_types`
+FOREIGN KEY (`product_type_id`) REFERENCES `product_types` (`id`);
