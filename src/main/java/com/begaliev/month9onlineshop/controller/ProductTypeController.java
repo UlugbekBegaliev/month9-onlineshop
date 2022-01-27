32f327b291f755019dbbffc091fe14087604317b
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-27 16:32:12 +0600

    added ProductTypeController

--- src/main/java/com/begaliev/month9onlineshop/controller/ProductTypeController.java
+++ src/main/java/com/begaliev/month9onlineshop/controller/ProductTypeController.java
@@ -1,7 +1,8 @@
 package com.begaliev.month9onlineshop.controller;
 
-import com.begaliev.month9onlineshop.dto.ProductDTO;
+import com.begaliev.month9onlineshop.dto.ProductTypeDTO;
 import com.begaliev.month9onlineshop.service.ProductService;
+import com.begaliev.month9onlineshop.service.ProductTypeService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,14 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/products")
-public class ProductController {
+@RequestMapping("/api/product-types")
+public class ProductTypeController {
 
     private final ProductService productService;
+    private final ProductTypeService productTypeService;
 
     @GetMapping
-    public List<ProductDTO> getAll(Pageable pageable){
-        return productService.getAll(pageable).getContent();
+    public List<ProductTypeDTO> getAll(Pageable pageable){
+        return productTypeService.getAll(pageable).getContent();
     }
 }
