64e9a6748a57da99f0c51eb52cb1fe5923e444de
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-31 15:27:35 +0600

    added values in customer model

--- src/main/java/com/begaliev/month9onlineshop/model/Customer.java
+++ src/main/java/com/begaliev/month9onlineshop/model/Customer.java
@@ -1,17 +1,17 @@
 package com.begaliev.month9onlineshop.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Table(name = "customers")
 public class Customer {
 
@@ -19,12 +19,25 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Email
+    @NotBlank
+    @Size(min = 1, max = 128)
     @Column(length = 128)
     private String email;
 
+    @NotBlank
+    @Size(min = 8, max = 128)
     @Column(length = 128)
     private String password;
 
+    @NotBlank
+    @Size(min = 1, max = 128)
     @Column(length = 128)
     private String fullname;
+
+    @NotBlank
+    @Builder.Default
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    private String role = "USER";
 }
