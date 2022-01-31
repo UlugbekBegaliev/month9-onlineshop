b2ae7be00f85c5d625569315890a792daaaec776
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-31 16:07:58 +0600

    added ProductNotFoundException class

---
+++ src/main/java/com/begaliev/month9onlineshop/exeption/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.begaliev.month9onlineshop.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ProductNotFoundException extends RuntimeException {
+
+}
