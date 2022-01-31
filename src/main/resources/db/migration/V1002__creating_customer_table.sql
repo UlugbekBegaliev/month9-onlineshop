56738243e32d4e24b3237cdcc3a55a0d28f252e5
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-01-31 17:12:21 +0600

    added values in customer migration file

--- src/main/resources/db/migration/V1002__creating_customer_table.sql
+++ src/main/resources/db/migration/V1002__creating_customer_table.sql
@@ -10,3 +10,13 @@ CREATE TABLE `customers` (
      PRIMARY KEY (`id`),
      UNIQUE INDEX `email_unique` (`email` ASC)
 );
+
+CREATE TABLE `baskets` (
+    `id` INT auto_increment NOT NULL,
+    `product_name` VARCHAR(128) NOT NULL,
+    `quantity` FLOAT NOT NULL,
+    `price` FLOAT NOT NULL,
+    `customer_id` INT NOT NULL,
+    FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+    PRIMARY KEY (`id`)
+)
