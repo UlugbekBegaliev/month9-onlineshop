eaab10b8ab617693889ed59ff2be2e6f5f1d50b2
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-07 15:38:24 +0600

    added new values

--- src/main/java/com/begaliev/month9onlineshop/service/PurchaseService.java
+++ src/main/java/com/begaliev/month9onlineshop/service/PurchaseService.java
@@ -1,4 +1,17 @@
 package com.begaliev.month9onlineshop.service;
 
+import com.begaliev.month9onlineshop.repository.BasketRepository;
+import com.begaliev.month9onlineshop.repository.CustomerRepository;
+import com.begaliev.month9onlineshop.repository.PurchaseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PurchaseService {
+
+    private final PurchaseRepository purchaseRepository;
+    private final CustomerRepository customerRepository;
+    private final BasketRepository basketRepository;
+
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const baseUrl = 'http://localhost:8080';
+const restUrl = 'http://localhost:8080/api';
+
