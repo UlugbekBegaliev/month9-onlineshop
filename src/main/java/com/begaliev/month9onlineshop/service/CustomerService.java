6ba3be46ac99c8d86d4bcaa88066a0018aa7a017
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-07 15:13:36 +0600

    added CustomerService

--- src/main/java/com/begaliev/month9onlineshop/service/CustomerService.java
+++ src/main/java/com/begaliev/month9onlineshop/service/CustomerService.java
@@ -1,11 +1,15 @@
 package com.begaliev.month9onlineshop.service;
 
 import com.begaliev.month9onlineshop.dto.CustomerDTO;
+import com.begaliev.month9onlineshop.exeption.CustomerAlreadyRegisteredException;
+import com.begaliev.month9onlineshop.exeption.CustomerNotFoundException;
+import com.begaliev.month9onlineshop.model.Customer;
 import com.begaliev.month9onlineshop.model.CustomerRegisterForm;
 import com.begaliev.month9onlineshop.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +17,30 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder encoder;
 
     public Page<CustomerDTO> getAll(Pageable pageable){
         return customerRepository.findAll(pageable).map(CustomerDTO::from);
     }
 
     public CustomerDTO register(CustomerRegisterForm form) {
-        return CustomerDTO.from();
+        if (customerRepository.existsByEmail(form.getEmail())){
+            throw new CustomerAlreadyRegisteredException();
+        }
+
+        var user = Customer.builder()
+                .email(form.getEmail())
+                .fullname(form.getName())
+                .password(encoder.encode(form.getPassword()))
+                .build();
+
+        return CustomerDTO.from(user);
+    }
+
+    public CustomerDTO getByEmail(String email) {
+        var user = customerRepository.findByEmail(email)
+                .orElseThrow(CustomerNotFoundException::new);
+
+        return CustomerDTO.from(user);
     }
 }
