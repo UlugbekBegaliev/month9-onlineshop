66fa2d5828d2bb045c5bccff57041f3dc9a7138e
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-10 15:49:52 +0600

    added class Constants

--- src/main/java/com/begaliev/month9onlineshop/fronted/BasketController.java
+++ src/main/java/com/begaliev/month9onlineshop/fronted/BasketController.java
@@ -3,10 +3,12 @@ package com.begaliev.month9onlineshop.fronted;
 import com.begaliev.month9onlineshop.repository.BasketRepository;
 import com.begaliev.month9onlineshop.service.BasketService;
 import com.begaliev.month9onlineshop.service.ProductService;
+import com.begaliev.month9onlineshop.service.PurchaseService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @AllArgsConstructor
@@ -14,7 +16,8 @@ public class BasketController {
 
     private final BasketService basketService;
     private final ProductService productService;
+    private final PurchaseService purchaseService;
 
-//    @GetMapping
-//    public String basket(Model model, )
+
+
 }
---
+++ src/main/java/com/begaliev/month9onlineshop/model/Constants.java
@@ -0,0 +1,6 @@
+package com.begaliev.month9onlineshop.model;
+
+public final class Constants {
+
+    public static final String BASKET_ID = "_basket_";
+}
