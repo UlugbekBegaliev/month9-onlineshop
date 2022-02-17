fac4134329e90f51a77c352f2753b6b41fb851dd
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-17 20:56:05 +0600

    updated migration file product_type

--- src/main/resources/db/migration/V1001__creating_table_product_type.sql
+++ src/main/resources/db/migration/V1001__creating_table_product_type.sql
@@ -10,3 +10,9 @@ use `onlineshop`;
 ALTER TABLE `products`
 ADD COLUMN `product_type_id` INT NOT NULL after `image`, ADD CONSTRAINT `fk_product__product_types`
 FOREIGN KEY (`product_type_id`) REFERENCES `product_types` (`id`);
+
+INSERT INTO `product_types` (`name`, `icon`) VALUES
+('Audi', 'audi.png'),
+('BMW', 'bmw.jpg'),
+('Mercedes', 'mers.png'),
+('Toyota', 'toyota.png');
