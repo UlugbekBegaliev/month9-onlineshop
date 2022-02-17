b90ef53d68df1a6d483b202dfffaee5a6dba34dd
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-17 20:40:15 +0600

    added values in new class

--- src/main/java/com/begaliev/month9onlineshop/controller/AdviceController.java
+++ src/main/java/com/begaliev/month9onlineshop/controller/AdviceController.java
@@ -1,9 +1,20 @@
 package com.begaliev.month9onlineshop.controller;
 
+import com.begaliev.month9onlineshop.model.Constants;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class AdviceController {
 
-
+    public List<String> getBasketModel(HttpSession httpSession) {
+        var list = httpSession.getAttribute(Constants.BASKET_ID);
+        if (list == null) {
+            httpSession.setAttribute(Constants.BASKET_ID, new ArrayList<>());
+        }
+        return (List<String>) httpSession.getAttribute(Constants.BASKET_ID);
+    }
 }
