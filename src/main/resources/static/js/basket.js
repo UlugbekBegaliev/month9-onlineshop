c02575e0ed7e45a7671db54a7e9e441cb13e774d
UlugbekBegaliev <uluga.begaliev@gmail.com>
2022-02-17 23:31:25 +0600

    added new values

--- src/main/resources/static/js/basket.js
+++ src/main/resources/static/js/basket.js
@@ -2,3 +2,46 @@
 
 const baseUrl = 'http://localhost:8080';
 const csrfToken = document.querySelector('[name=_csrf_token]').getAttribute('content');
+
+function deleteOne() {
+    $(".delete").click(async function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let id = $(this).parent().parent().attr('id');
+        const data = { id: id };
+
+        const options = {
+            method : 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+            body: JSON.stringify(data)
+        };
+        await fetch(baseUrl+"/basket/id", options);
+        window.location.href = baseUrl + "/basket";
+    });
+}
+function buyOne() {
+    $(".buy").click(async function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let id = $(this).parent().parent().attr('id');
+        const data = { id: id };
+
+        const options = {
+            method : 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            },
+            body: JSON.stringify(data)
+        };
+        await fetch(baseUrl+"/purchase/id", options);
+        window.location.href = baseUrl + "/purchases";
+    });
+}
+deleteOne();
+buyOne();
